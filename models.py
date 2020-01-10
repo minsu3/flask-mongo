a737f338c6402bb78d522f689f14472b7cca0d38
Min Su Kim <kim97minsu@gmail.com>
2020-01-10 14:04:33 -0800

    created login and users route

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from app import app, mongo 
 from flask import jsonify
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 from werkzeug.security import generate_password_hash
 
 class Users:
@@ -13,3 +15,21 @@ class Users:
         resp = jsonify({ 'msg': 'User created successfully' })
         resp.status_code = 200
         return resp
+    
+    def get_one_by_credentials(data):
+        user = mongo.db.user.find_one({ 'email': data['email'] })
+        return dumps(user)
+    
+    def get_all():
+        users = mongo.db.user.find()
+        return dumps(users)
+    
+    def delete_one(user_id):
+        user = mongo.db.user.find_one({ '_id': Object(user_id) })
+        if user:
+            mongo.db.user.delete_one({ '_id': ObjectId(user_id) })
+            return jsonify( 'msg': 'User deleted successfully!' )
+        else: 
+            return jsonify({ 'msg': 'A user with that {} could not be found'.format(user_id) })
+        
+        
